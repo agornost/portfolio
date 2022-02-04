4a2f2254672ef302d567ffa81d909eef5ad340f7
Andrey Gornostaev <Gara@Andreys-MacBook-Air.local>
2022-02-04 10:23:04 -0400

    Week 2 updates

--- week 2/passwordCheck.sh
+++ week 2/passwordCheck.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
-read -s -p "Type secret password:" passwd
+NC='\033[0m'
+col_red=$'\e[1;31m'
+col_green=$'\e[1;32m'
+read -s -p "${col_red}Type secret password:" passwd
+printf $NC
 echo
 hash=`echo -n $passwd | shasum -a 256`
 #echo $hash
 
 if grep -q "$hash" "secret.txt"; then
-	echo "Access Granted"
+	echo "${col_green}Access Granted"
+	printf $NC
 	exit 0
 else
 	echo "Access Denied"
