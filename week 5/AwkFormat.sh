38a6588b1745a985da895c27679851887e04e14b
Andrey Gornostaev <Gara@Andreys-MacBook-Air.local>
2022-02-15 12:59:38 -0400

    Add Week 5 scripts to portfolio

---
+++ week 5/AwkFormat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Google Server IPs:"
+# awk '{print $1}' input.txt
+awk 'BEGIN {
+    FS=":";
+    print "________________________________";
+    print "| \033[34mServer Type\033[0m | \033[34mIP             \033[0m |";
+}
+{
+    printf("| \033[33m%-11s\033[0m | \033[35m%-15s\033[0m |\n", $1, $2);
+}
+END {
+    print("________________________________");
+}' input.txt
---
+++ week 5/regexes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+grep -r ' sed ' ./*/                #all sed statements
+grep -r '^m' ./*/                   #all lines that start with the letter m
+grep -r '\<[0-9]\{3\}\>' ./*/       #all lines that contain three digit numbers
+grep -r 'echo.*(\S+\s+){3}.*' ./*/  #all echo statements with at least three words
+grep -r '^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$
+'
+# 8 characters length
+# 2 letters in Upper Case
+# 1 Special Character (!@#$&*)
+# 2 numerals (0-9)
+# 3 letters in Lower Case
