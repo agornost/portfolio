992715ce9f4a852ca2685eea9c8e1ea28f13080d
Andrey Gornostaev <Gara@Andreys-MacBook-Air.local>
2022-02-18 13:52:09 -0500

    Add Week 5 scripts to portfolio

--- week 5/regexes.sh
+++ week 5/regexes.sh
@@ -4,7 +4,7 @@ grep -r ' sed ' ./*/                #all sed statements
 grep -r '^m' ./*/                   #all lines that start with the letter m
 grep -r '\<[0-9]\{3\}\>' ./*/       #all lines that contain three digit numbers
 grep -r 'echo.*(\S+\s+){3}.*' ./*/  #all echo statements with at least three words
-grep -r '^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$
+grep -r '^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$'
 '
 # 8 characters length
 # 2 letters in Upper Case
