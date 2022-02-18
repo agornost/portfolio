52776f9c23558fa9513dc48fe975f0d21bec525a
Andrey Gornostaev <Gara@Andreys-MacBook-Air.local>
2022-02-18 08:38:45 -0500

    Add Week 2 scripts to portfolio

--- week 2/passwordCheck.sh
+++ week 2/passwordCheck.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 # note for course facilitator: password is qwerty
-NC='\033[0m'
-col_red=$'\e[1;31m'
-col_green=$'\e[1;32m'
+
+NC='\033[0m' 						#set default terminal font colour
+col_red=$'\e[1;31m'			#set red terminal font colour
+col_green=$'\e[1;32m'		#set green terminal font colour
 read -s -p "${col_red}Type secret password:" passwd
 printf $NC
 echo
+
+#hashing of input password
 hash=`echo -n $passwd | shasum -a 256`
+
+#conditional check whether hash of input password matches the one in secret.txt
 if grep -q "$hash" "secret.txt"; then
 	echo "${col_green}Access Granted"
 	printf $NC
