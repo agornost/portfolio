014abcdc3b28991470f41074d9b6f8dbc48d5227
Andrey Gornostaev <Gara@Andreys-MacBook-Air.local>
2022-02-18 08:32:33 -0500

    Add Week 1 scripts to portfolio

--- week 1/second.sh
+++ week 1/second.sh
@@ -1,8 +1,6 @@
-#!/bin/bash 
-      
-echo "Hi there!" 
-      
-echo "It's good to see you $1!" 
-      
-exit 0 
-     
+#!/bin/bash
+echo "Hi there!"
+
+#Following echo prints greeting with name as input argument
+echo "It's good to see you $1!"
+exit 0
